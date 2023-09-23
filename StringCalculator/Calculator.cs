4e260a2dd6dd0973fd1dd0a33950ea53d062e0fe
Lara Penberthy <lara.baker96@yahoo.com>
2023-09-23 10:17:05 +0200

    initial commit

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add1(string numbers)
+        {
+            int result = 0;
+
+            if(numbers != "")
+            {
+                foreach (string number in numbers.Split(","))
+                {
+                    result += Convert.ToInt32(number);
+                }
+
+            }
+
+            return result;
+        }
+
+        //Same as number 1 as used a method in string 1 that didnt restrict me to 2 numbers
+        public static int Add2(string numbers)
+        {
+            int result = 0;
+
+            if (numbers != "")
+            {
+                foreach (string number in numbers.Split(","))
+                {
+                    result += Convert.ToInt32(number);
+                }
+
+            }
+
+            return result;
+        }
+
+        public static int Add3(string numbers)
+        {
+            int result = 0;
+
+            if (numbers != "")
+            {
+                string[] seperators = new string[] { ",", "\\n" };
+                foreach (string number in numbers.Split(seperators, StringSplitOptions.None))
+                {
+                    result += Convert.ToInt32(number);
+                }
+
+            }
+
+            return result;
+        }
+
+        public static int Add4(string numbers)
+        {
+            int result = 0;
+
+            if (numbers != "")
+            {
+                string[] seperators = new string[] { ",", "\\n" };
+
+                //You want to check if it must start with // and then change the delimeters before the new line
+                //and then make numbers the value after the first \n
+                if (numbers.StartsWith("//"))
+                {
+                    seperators = new string[] { numbers[2].ToString() };
+
+                    numbers = numbers.Substring(numbers.IndexOf("\\n") + 2);
+                }
+
+                foreach (string number in numbers.Split(seperators, StringSplitOptions.None))
+                {
+                    result += Convert.ToInt32(number);
+                }
+
+            }
+
+            return result;
+        }
+
+        public static int Add5(string numbers)
+        {
+            int result = 0;
+
+            if (numbers != "")
+            {
+                string[] seperators = new string[] { ",", "\\n" };
+
+                if (numbers.StartsWith("//"))
+                {
+                    seperators = new string[] { numbers[2].ToString() };
+
+                    numbers = numbers.Substring(numbers.IndexOf("\\n") + 2);
+                }
+
+                //TODO: Return negatives string with console log error
+                string negatives = "";
+                foreach (string number in numbers.Split(seperators, StringSplitOptions.None))
+                {
+                    if (Convert.ToInt32(number) > 0)
+                    {
+                        result += Convert.ToInt32(number);
+                    }
+                    else if (Convert.ToInt32(number) < 0)
+                    {
+                        negatives += number;
+                    }
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ StringCalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringCalculator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Task 1: Please insert a string of up to 2 numbers");
+
+        //string input = Console.ReadLine();
+
+        //int result = Calculator.Add1(input);
+
+        //Console.WriteLine("Sum of string is: " + result);
+
+        //Console.WriteLine("Task 2: Please insert a string of any numbers");
+
+        //string input2 = Console.ReadLine();
+
+        //int result2 = Calculator.Add2(input2);
+
+        //Console.WriteLine("Sum of string is: " + result2);
+
+        //Console.WriteLine("Task 3: Please insert a string of any numbers");
+
+        //string input3 = Console.ReadLine();
+
+        //int result3 = Calculator.Add3(input3);
+
+        //Console.WriteLine("Sum of string is: " + result3);
+
+        //Console.WriteLine(@"Task 4: Please insert a string of any numbers, you can specify the delimiter by inserting //Delimeter\n before.");
+
+        //string input4 = Console.ReadLine();
+
+        //int result4 = Calculator.Add4(input4);
+
+        //Console.WriteLine("Sum of string is: " + result4);
+
+        Console.WriteLine(@"Task 5: Please insert a string of any numbers, you can specify the delimiter by inserting //Delimeter\n before.");
+
+        string input5 = Console.ReadLine();
+
+        int result5 = Calculator.Add5(input5);
+
+        Console.WriteLine("Sum of string is: " + result5);
+
+
+    }
+}
