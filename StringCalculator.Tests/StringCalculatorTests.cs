1c0526a557c392ae68487d8da149de53ad169bce
Lara Penberthy <lara.baker96@yahoo.com>
2023-12-27 17:02:23 +0200

    setup calculator and test classes with empty string check

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        private StringCalculator _calculator = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void Given_EmptyString_Returns_Zero()
+        {
+            string testInput = "";
+            int expectedOutput = 0;
+
+            var calculatedOutput = _calculator.Add(testInput);
+
+            Assert.AreEqual(expectedOutput, calculatedOutput);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbersGiven)
+        {
+            int finalResult = 0;
+
+            string numbersToCalculate = numbersGiven;
+
+            if (String.IsNullOrEmpty(numbersToCalculate))
+            {
+                return 0;
+            }
+
+            return finalResult;
+        }
+    }
+}
